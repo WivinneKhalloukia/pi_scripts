42494da139e1df29cfa78c3cebc19fc8ffa3e0cc
Wivinne Khalloukia <r1045049@student.thomasmore.be>
2025-02-24 12:35:30 +0100

    blink.py

--- blink.py
+++ blink.py
@@ -6,9 +6,9 @@ import sys
 # Define a function called blink that takes a pin number as an argument
 def blink(_pin):
    wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin, which turns on the LED
-   time.sleep(0.1)                   # Wait for 0.5 seconds
+   time.sleep(0.0)                   # Wait for 0.5 seconds
    wiringpi.digitalWrite(_pin, 0)    # Write 0 ( LOW ) to pin, which turns off the LED
-   time.sleep(0.1)                   # Wait for another 0.5 seconds
+   time.sleep(0.0)                   # Wait for another 0.5 seconds
 
 # Initialize the wiringpi library and set pin 2 to output mode
 print("Start")                        # Print "Start" to the console
@@ -17,7 +17,7 @@ wiringpi.wiringPiSetup()              # Initialize the wiringpi library
 wiringpi.pinMode(pin, 1)              # Set pin 2 to mode 1 ( OUTPUT ), which means it can send signals
 
 # Blink the LED connected to pin 2 for 10 times
-for i in range(0,35):                 # Create a loop that repeats 10 times
+for i in range(0,100):                 # Create a loop that repeats 10 times
    blink(pin)                        # Call the blink function with pin 2 as the argument
 
 # Print "Done" to the console when the loop is finished
